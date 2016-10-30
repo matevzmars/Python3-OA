d6dfd869e42173e7f153540be5f1f0151672564c
Matevž Marš <matevz.mars@gmail.com>
2016-10-30 21:47:16 +0100

    Kaj se moramo še naučiti od 26.9.

---
+++ kaj_se_moramo_nauciti.py
@@ -0,0 +1,23 @@
+##7. naloga: WHILE zanka
+##Dva štejeta, s tem, da eden začne pri 15, drugi pa 30. Prvi bo
+##nehal štet, ko pride do 25, drugi pa 45. Ko eden neha štet, neha tudi drugi.
+##Pri katerih številkah se bosta ustavila? Izpiši štetje.
+###############################################################
+##Risanje: Turtle
+##import turtle
+##t=turtle.Pen()
+##t.forward()
+##t.backward()
+##t.left()
+##t.right()
+##t.up()
+##t.down()
+##t.reset()
+##t.clear()
+###############################################################
+##8. naloga: turtle
+##Nariši pravokotnik, trikotnik, kvadrat brez vogalov. Kako lahko še narišeš
+##kvadrat brez vogalov? Nariši hišo, ki ima vrata in dve okni. Nariši svoje ime.
+###############################################################
+
+
