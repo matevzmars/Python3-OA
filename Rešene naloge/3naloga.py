a08ea8d53ae02cd898f23e0543ec67586ee185af
Matevž Marš <matevz.mars@gmail.com>
2016-10-30 21:48:31 +0100

    Rešena 3. naloga

---
+++ Rešene naloge/3naloga.py
@@ -0,0 +1,2 @@
+slovar={'Najljubša hrana':'Pica','Najljubša risanka':'Tom &Jerry','Najljubši film':'Star Wars','Najljubša knjiga':'Štoparski vodnik po galaksiji','Najljubši predmet':'Fizika','Najljubši hobi':'Nogomet'}
+print(slovar['Najljubši predmet'])
