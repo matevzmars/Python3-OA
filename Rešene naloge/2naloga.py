7a3b68b04e0666da6b491a253185f57f319b2ed8
Matevž Marš <matevz.mars@gmail.com>
2016-10-30 21:48:23 +0100

    Rešena 2. naloga

---
+++ Rešene naloge/2naloga.py
@@ -0,0 +1,7 @@
+seznamImen=('Tilen','Tim','Amon','Nal','Luka','Tim','Lev','Žiga','Martin','Matevž')
+print(seznamImen[0])
+print(seznamImen[2:6])
+seznamImen.append('Janez')
+del seznamImen[3]
+print(seznamImen)
+
