766b8f48bbf768235db6a4e5b2c7898165cf05c9
Matevž Marš <matevz.mars@gmail.com>
2016-10-30 21:46:22 +0100

    Besedila nalog, rešenih do 9.11.

---
+++ besedila_nalog.py
@@ -0,0 +1,39 @@
+1. naloga: ponovitev 1. ure
+Napiši program, ki te vpraša po dveh številkah
+in ti po vrsticah vrne vsoto, razliko, kvocient, produkt,
+celoštevilski kvocient pri deljenju in ostanek pri deljenju
+vnešenih številk.
+###############################################################
+2. naloga: seznam
+Napišimo seznam z imeni udeležencev. Izpiši ime prvega v seznamu.
+Izpiši imena tretjega do šestega udeleženca. Dodaj novega udeleženca.
+Izbriši četrtega udeleženca in izpiši nov seznam udeležencev.
+###############################################################
+3. naloga: zemljevid/slovar
+Vsak naj napiše slovar v katerega bo vpisal svojo najljubšo hrano,
+risanko, film, knjigo, predmet, hobi,...
+Izpiši najljubši predmet.
+###############################################################
+4. naloga: pogojni stavek
+Napiši program, ki te vpraša koliko si
+star in te uvrsti v kategorijo:
+dojenček 1,
+otrok    14,
+najstnik 19,
+mladostnik 30,
+odrasel človek 55,
+starček 120,
+starejši od 120 let.
+###############################################################
+5. naloga: FOR zanka
+a)Napiši poljubni seznam in izpiši elemente seznama s for zanko.
+b)Kaj naredi program:
+for i in seznam:
+    print(i)
+    for j in seznam:
+        print(j)
+c)for i in range(0,len(seznam)):
+    print(seznam[i])
+###############################################################
+6. naloga: WHILE zanka
+Oštevilči svoj seznam. Naredi tudi s FOR zanko.
