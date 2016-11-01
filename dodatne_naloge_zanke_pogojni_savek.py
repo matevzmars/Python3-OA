1f6856a3ac9350d890e5e36abcd9e734f9c2ee4a
Matevž Marš <matevz.mars@gmail.com>
2016-11-01 13:23:10 +0100

    napisane naloge za vaje

---
+++ dodatne_naloge_zanke_pogojni_savek.py
@@ -0,0 +1,27 @@
+1. naloga:
+Izpiši števila, ki so deljiva s 7 in so večkratniki števila 5, med 
+števili 1500 in 2700.
+
+2. naloga:
+Napiši program, v katerem uporabnik ugiba število med 1 in 100. 
+Rešuj z metodo bisekcije. Prvi dve vrstici programa naj vsebujeta:
+from random import *
+a=randint(1,100)
+
+3. naloga:
+Napiši program, ki uporabnika vpraša po besedi in jo izpiše v obratnem
+vrstnem redu.
+
+4. naloga:
+Napiši program, ki uporabnika vpraša po številki in izpiše vsoto vseh 
+naravnih števil, da vnešene številke.
+
+5. naloga:
+Napiši program, ki uporabnika vpraša po številki in izpiše večkratnike 
+tega števila.
+Primer:
+Vpiši število: 6
+6 x 1 = 6
+6 x 2 = 12
+6 x 3 = 18
+...
