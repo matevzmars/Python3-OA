45c34647f3a6e950b5ac24baa04caf88e7edd1cf
Matevž Marš <matevz.mars@gmail.com>
2016-11-24 07:55:04 +0100

    kaj smo se do 23.11.2016 naučili

--- kaj_ze_znamo.py
+++ kaj_ze_znamo.py
@@ -84,3 +84,35 @@ if + break
 while True:
     print(a)
 ###############################################################
+Lego Mindstorms
+
+http://python-ev3dev.readthedocs.io/en/latest/spec.html
+https://sites.google.com/site/ev3python/
+
+#!/usr/bin/env python3
+import ev3dev.ev3 as ev3
+###############################################################
+Motorji
+m=ev3.LargeMotor('outA')
+
+#neskončno vrtenje
+m.run_forever(speed_sp=100) #speed_sp je od 0 do 900
+m.stop(stop_action='brake') #stop_action je lahko 'brake' ali 'coast'
+
+#vrtenje za določen kot
+m.run_to_rel_pos(position_sp=360,speed_sp=100) 
+
+#neskončno vrtenje, kjer lahko vmes spreminjaš hitrost
+m.run_direct(duty_cycle_sp=20) #duty_cycle je od 0 do 100
+m.duty_cycle_sp=30
+
+#vrtenje za določen čas
+m.run_timed(time_sp=1000,speed_sp=100) #čas je v milisekundah
+
+m.reset()
+
+m.duty_cycle_sp=50
+m.duty_cycle_sp
+
+from time import sleep
+sleep(5) #počaka 5 sekund
