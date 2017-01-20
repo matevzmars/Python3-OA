11c7adb8bb210fb76f7e6b38ce89e5d2b74cdc8b
Matevž Marš <matevz.mars@gmail.com>
2017-01-20 22:32:52 +0100

    popravljen program, da res počaka 1 sekundo

--- Lego Mindstorms/ravnoGibanje.py
+++ Lego Mindstorms/ravnoGibanje.py
@@ -7,24 +7,22 @@ from time import sleep
 m1=ev3.LargeMotor('outA') #prvi motor smo povezali na A izhod
 m2=ev3.LargeMotor('outB') #drugi motor smo povezali na B izhod
 
-m1.run_to_rel_pos(position_sp=360*2,speed_sp=500) #motor 1 se premakne za dva obrata
-m2.run_to_rel_pos(position_sp=360*2,speed_sp=500) #motor 2 se premakne za dva obrata
+m1.run_to_rel_pos(position_sp=360*2,speed_sp=500, stop_action='brake') #motor 1 se premakne za dva obrata
+m2.run_to_rel_pos(position_sp=360*2,speed_sp=500, stop_action='brake') #motor 2 se premakne za dva obrata
 
-m1.wait_while('running') #počakaj dokler se motor 1 premika
-m2.wait_while('running') #počakaj dokler se motor 2 premika
-
-sleep(1) #počakamo 1 sekundo
-
-m1.run_to_rel_pos(position_sp=-720,speed_sp=500) #motor 1 se premakne za 720 stopinj v drugo smer
-m2.run_to_rel_pos(position_sp=-720,speed_sp=500) #motor 2 se premakne za 720 stopinj v drugo smer
-
-m1.wait_while('running') #počakaj dokler se motor 1 premika
-m2.wait_while('running') #počakaj dokler se motor 2 premika
+while(len(m1.state)>0 or len(m2.state)>0): #počakaj dokler se motor 1 ali motor 2 premika
+	i=0										#neumen ukaz
 
 sleep(1) #počakamo 1 sekundo
 
-m1.run_timed(time_sp=1000,speed_sp=500) #motor 1 deluje 1 sekundo
-m2.run_timed(time_sp=1000,speed_sp=500) #motor 2 deluje 1 sekundo
+m1.run_to_rel_pos(position_sp=-720,speed_sp=500, stop_action='brake') #motor 1 se premakne za 720 stopinj v drugo smer
+m2.run_to_rel_pos(position_sp=-720,speed_sp=500, stop_action='brake') #motor 2 se premakne za 720 stopinj v drugo smer
 
-m1.wait_while('running') #počakaj dokler se motor 1 premika
-m2.wait_while('running') #počakaj dokler se motor 2 premika
+while(len(m1.state)>0 or len(m2.state)>0): #počakaj dokler se motor 1 ali motor 2 premika
+	i=0										#neumen ukaz
+
+sleep(1) #počakamo 1 sekundo
+
+m1.run_timed(time_sp=1000,speed_sp=500, stop_action='brake') #motor 1 deluje 1 sekundo
+m2.run_timed(time_sp=1000,speed_sp=500, stop_action='brake') #motor 2 deluje 1 sekundo
+
